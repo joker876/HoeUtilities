f1a35266a115f5aadc2fc393742968073ddc0b2a
joker876 <42680429+joker876@users.noreply.github.com>
2020-12-14 17:25:23 +0100

    updated constants

--- helperFunctions/constants.js
+++ helperFunctions/constants.js
@@ -104,7 +104,7 @@ export const dataFileStructure = {
     key: null,
     hud: { x: 20, y: 5 }, 
     timer: { x: Renderer.screen.getWidth() - 40, y: 5 },
-    session: { x: Renderer.screen.getWidth() - 100, y: 5 }
+    farming: { x: Renderer.screen.getWidth() - 175, y: 5 }
 }
 
 export const imageCane = new Image("hoeutils_sugar_cane","https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/58/Sugar_Cane_%28item%29_JE1_BE1.png")
@@ -113,7 +113,6 @@ export const imageCarrot = new Image('hoeutils_carrot', 'https://static.wikia.no
 export const imageWheat = new Image('hoeutils_wheat', 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/ef/Wheat_JE1_BE1.png')
 export const imageWart = new Image('hoeutils_wart', 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/c/c0/Nether_Wart_%28item%29_JE1.png')
 export const imageEvent = new Image('hoeutils_event', 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/ef/Wheat_JE1_BE1.png')
-export const imageFarming = new Image('hoeutils_farming', 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/9/9d/Golden_Hoe_JE3_BE3.png')
 
 export const collections = {
     last_updated: 0,
@@ -125,6 +124,8 @@ export const collections = {
     pumpkin: 0,
     melon: 0,
     cocoa: 0,
+    mushroom: 0,
+    cactus: 0,
 }
 
 export const skillCurves = [50, 175, 375, 675, 1175, 1925, 2925, 4425, 6425, 9925, 14925, 22425, 32425, 47425, 67425, 97425, 147425, 222425, 322425, 522425, 822425, 1222425, 1722425, 2322425, 3022425, 3822425, 4722425, 5722425, 6822425, 8022425, 9322425, 10722425, 12222425, 13822425, 15522425, 17322425, 19222425, 21222425, 23322425, 25522425, 27822425, 30222425, 32722425, 35322425, 38072425, 40972425, 44072425, 47472425, 51172425, 55172425, 59472425, 64072425, 68972452, 74172425, 79672425, 85472425, 91572425, 97972425, 104672425, 111672425];
