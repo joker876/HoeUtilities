0a75a83ef56d846dff23c14452a695f69efbe21d
joker876 <42680429+joker876@users.noreply.github.com>
2020-12-14 17:28:03 +0100

    removed farming image, improved offset calculation

--- helperFunctions/renderOverlays.js
+++ helperFunctions/renderOverlays.js
@@ -10,12 +10,16 @@ export function standardImages() {
     //main
     function countActiveModules() {
         const heldItem = Player.getHeldItem().getItemNBT().getCompoundTag('tag').getCompoundTag('ExtraAttributes');
-        let count = 0
-        if (global.hoeutils.userSettings.isCropRateEnabled) count++;
-        if (global.hoeutils.userSettings.isCounterEnabled && !heldItem.getString('id').match(/PUMPKIN|MELON|COCOA/)) count++;
-        if (global.hoeutils.userSettings.isMaxEfficiencyEnabled) count++;
-        if (global.hoeutils.userSettings.isCollectionEnabled && !heldItem.getString('id').match(/HOE_WARTS/)) count++;
-        return count;
+        return global.hoeutils.userSettings.isCropRateEnabled
+        + (global.hoeutils.userSettings.isCounterEnabled && !heldItem.getString('id').match(/PUMPKIN|MELON|COCOA/))
+        + global.hoeutils.userSettings.isMaxEfficiencyEnabled
+        + global.hoeutils.userSettings.isCollectionEnabled
+        + (
+            global.hoeutils.userSettings.isCropRateEnabled
+            || (global.hoeutils.userSettings.isCounterEnabled && !heldItem.getString('id').match(/PUMPKIN|MELON|COCOA/))
+            || global.hoeutils.userSettings.isMaxEfficiencyEnabled
+            || global.hoeutils.userSettings.isCollectionEnabled
+        )
     }
     global.hoeutils.imageData = {
         enabled: false,
@@ -37,21 +41,8 @@ export function standardImages() {
         case 1:
             global.hoeutils.imageData.yOffset = -3 * global.hoeutils.scale;
             break;
-        case 2:
-            global.hoeutils.imageData.yOffset = -2 * global.hoeutils.scale;
-            break;
-        case 3:
-            global.hoeutils.imageData.yOffset = 3 * global.hoeutils.scale;
-            break;
-        case 4:
-            global.hoeutils.imageData.yOffset = 9 * global.hoeutils.scale;
-            break;
-        case 5:
-            global.hoeutils.imageData.yOffset = 15 * global.hoeutils.scale;
-            break;
-        case 6:
-            global.hoeutils.imageData.yOffset = 21 * global.hoeutils.scale;
-            break;
+        default:
+            global.hoeutils.imageData.yOffset = -4 + 6 * (activeModuleCount-2);
     }
     const heldItem = Player.getHeldItem().getItemNBT().getCompoundTag('tag').getCompoundTag('ExtraAttributes');
     if (!global.hoeutils.userSettings.isImageEnabled) return;
@@ -76,32 +67,5 @@ export function timerImage() {
     if (!Scoreboard.getTitle().replace(/§./g, '').equals('SKYBLOCK') && !global.hoeutils.userSettings.isEventTimerEnabledEverywhere) return;
     if (global.hoeutils.userSettings.isEventImageEnabled) {
         imageEvent.draw(global.hoeutils.data.timer.x - global.hoeutils.eventImageData.size - 4, global.hoeutils.data.timer.y + global.hoeutils.eventImageData.yOffset, global.hoeutils.eventImageData.size, global.hoeutils.eventImageData.size)
-    }
-}
-const imageFarming = new Image('hoeutils_farming', 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/9/9d/Golden_Hoe_JE3_BE3.png')
-export function farmingImage() {
-    if (!global.hoeutils.settings.getSetting('Features', 'Farming Info')) return;
-    if (!global.hoeutils.userSettings.farmingInfo.isImageEnabled) return;
-    function countActiveModules() {
-        const s = global.hoeutils.userSettings.farmingInfo;
-        return (s.isLevelEnabled || s.isExpPerHourEnabled || s.isTotalExpEnabled || s.isProgressToNextEnabled || s.isExpLeftEnabled || s.isETAToNextEnabled)
-        + s.isLevelEnabled + s.isExpPerHourEnabled + s.isTotalExpEnabled + s.isProgressToNextEnabled + s.isExpLeftEnabled + s.isETAToNextEnabled +
-        (s.isExpLeftToMaxEnabled || s.isProgressToMaxEnabled || s.isETAToMaxEnabled)
-        + s.isProgressToMaxEnabled + s.isExpLeftToMaxEnabled + s.isETAToMaxEnabled
-    }
-    const activeModuleCount = countActiveModules();
-    if (!activeModuleCount) return;
-    let size, yOffset;
-    switch (activeModuleCount) {
-        case 2:
-            size = 14 * global.hoeutils.scale;
-            break;
-        default:
-            size = 20 * global.hoeutils.scale;
-    }
-    switch (activeModuleCount) {
-        case value:
-            
-            break;
     }
 }
