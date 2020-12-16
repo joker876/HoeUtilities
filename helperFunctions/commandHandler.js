d20eaca855a66a160f2261010080b3bd1e72ec96
joker876 <42680429+joker876@users.noreply.github.com>
2020-12-16 11:18:25 +0100

    removed old command handler code

--- helperFunctions/commandHandler.js
+++ helperFunctions/commandHandler.js
@@ -3,33 +3,6 @@ const URI = Java.type("java.net.URI");
 import { printFeatureList } from './featureListMessage';
 import { helpMessages } from './constants';
 
-/* export function commandHandler(...args) {
-    if (!args || !args[0]) return ChatLib.command('hoeutilities', true);
-    if (args[0].toLowerCase() == 'gui') global.hoeutils.gui.open();
-    else if (args[0].toLowerCase() == 'help') printFeatureList();
-    else if (args[0].toLowerCase() == 'key') {
-        if (!args[1]) return ChatLib.chat('&cNo key specified! &fUse &b/api new &fto get one.');
-        if (!args[1].toLowerCase().match(/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/)) return ChatLib.chat('&cInvalid API key!');
-        global.hoeutils.data.key = args[1].toLowerCase();
-        ChatLib.chat('&a[HoeUtilities] &eyour API key has been set.');
-    }
-    else if (args[0].toLowerCase() == 'discord') {
-        if(Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-            Desktop.getDesktop().browse(new URI('https://discord.gg/UmpNYNRmGE'));
-        }
-    }
-    else if (args[0].toLowerCase() == 'testsound') {
-        World.playSound(args[1] || 'random.orb', args[2] || 1000, args[3] || 1);
-    }
-    else if (args[0].toLowerCase() == 'debug') {
-        FileLib.write('hoeutilities', './variableDumb.json', JSON.stringify(global.hoeutils[args[1]] ?? {state: 'undefined'}, null, 4));
-        console.log(JSON.stringify(global.hoeutils[args[1]] ?? {state: 'undefined'}));
-    }
-    else if (args[0].toLowerCase() == 'help') {
-
-    }
-    else ChatLib.chat('&eInvalid argument "'+args[0]+'".')
-} */
 export function commandHandler(...args) {
     let [ cmd, ...cmdData ] = args;
     
