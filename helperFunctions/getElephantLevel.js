4ef4daa7ffb50126e1bf6c81df0eec275f727f46
joker876 <42680429+joker876@users.noreply.github.com>
2020-12-16 16:19:56 +0100

    updated regex

--- helperFunctions/getElephantLevel.js
+++ helperFunctions/getElephantLevel.js
@@ -4,8 +4,8 @@ export function getElephantLevel() {
     let playerName = Player.getName();
     World.getAllEntities().forEach(pet => {
         pet = pet.getName();
-        if (ChatLib.removeFormatting(pet).match(new RegExp(`\\[Lv([0-9]{1,3})\\].+ ${playerName}'s Elephant`))) {
-            if (pet.match(new RegExp(`6${playerName}'s Elephant`))) {
+        if (ChatLib.removeFormatting(pet).match(new RegExp(`${playerName}'s Elephant`))) {
+            if (pet.match(new RegExp(`6${playerName}'s Elephant`)) || pet.match(new RegExp(`${playerName}§6's Elephant`))) {
                 global.hoeutils.elephantPetLevel = Number(ChatLib.removeFormatting(pet).match(/Lv(\d{1,3})/)[1]);
             }
         }
