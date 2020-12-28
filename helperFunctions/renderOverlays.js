3ae5d23701b9a609edb2e81dafd99e02fb1564ca
joker876 <42680429+joker876@users.noreply.github.com>
2020-12-28 17:55:26 +0100

    modified to use items instead of images, impr. perf.

--- helperFunctions/renderOverlays.js
+++ helperFunctions/renderOverlays.js
@@ -11,67 +11,45 @@ import {
     imageCactus,
     imageMushroom,
 } from './constants';
+import { countActiveModules } from './tickUpdates';
+countActiveModules()
 export function standardImages() {
     //main
-    function countActiveModules() {
-        const heldItem = Player.getHeldItem().getItemNBT().getCompoundTag('tag').getCompoundTag('ExtraAttributes');
-        return global.hoeutils.userSettings.isCropRateEnabled +
-            (global.hoeutils.userSettings.isCounterEnabled && !heldItem.getString('id').match(/PUMPKIN|MELON|COCOA/)) +
-            global.hoeutils.userSettings.isMaxEfficiencyEnabled +
-            global.hoeutils.userSettings.isCollectionEnabled +
-            (
-                global.hoeutils.userSettings.isCropRateEnabled ||
-                (global.hoeutils.userSettings.isCounterEnabled && !heldItem.getString('id').match(/PUMPKIN|MELON|COCOA/)) ||
-                global.hoeutils.userSettings.isMaxEfficiencyEnabled ||
-                global.hoeutils.userSettings.isCollectionEnabled
-            )
-    }
     global.hoeutils.imageData = {
-        enabled: false,
-        size: 0,
+        scale: 0,
         yOffset: 0,
     }
     if (!global.hoeutils.userSettings.isImageEnabled) return;
-    const activeModuleCount = countActiveModules()
-    if (!activeModuleCount) return;
-    switch (activeModuleCount) {
-        case 1:
-            global.hoeutils.imageData.size = 14 * global.hoeutils.scale;
-            break;
-        default:
-            global.hoeutils.imageData.size = 20 * global.hoeutils.scale;
-            break;
-    }
-    switch (activeModuleCount) {
-        case 1:
-            global.hoeutils.imageData.yOffset = -3 * global.hoeutils.scale;
-            break;
-        default:
-            global.hoeutils.imageData.yOffset = -4 + 6 * (activeModuleCount - 1);
-    }
+    
+    if (!global.hoeutils.activeModuleCount) return;
+
+    global.hoeutils.imageData.yOffset = -4 + 6 * (global.hoeutils.activeModuleCount - 1);
+    global.hoeutils.imageData.scale = 1.1 * global.hoeutils.scale;
+    global.hoeutils.imageData.size = 16 * global.hoeutils.scale;
+
     const heldItem = Player.getHeldItem().getItemNBT().getCompoundTag('tag').getCompoundTag('ExtraAttributes');
     if (!global.hoeutils.userSettings.isImageEnabled) return;
     try {
         if (heldItem.getString('id').match(/HOE_CANE/)) {
-            imageCane.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.size, global.hoeutils.imageData.size);
+            imageCane.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.scale);
         } else if (heldItem.getString('id').match(/HOE_POTATO/)) {
-            imagePotato.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.size, global.hoeutils.imageData.size);
+            imagePotato.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.scale);
         } else if (heldItem.getString('id').match(/HOE_CARROT/)) {
-            imageCarrot.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.size, global.hoeutils.imageData.size);
+            imageCarrot.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.scale);
         } else if (heldItem.getString('id').match(/HOE_WHEAT/)) {
-            imageWheat.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.size, global.hoeutils.imageData.size);
+            imageWheat.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.scale);
         } else if (heldItem.getString('id').match(/HOE_WARTS/)) {
-            imageWart.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.size, global.hoeutils.imageData.size);
+            imageWart.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.scale);
         } else if (heldItem.getString('id').match(/PUMPKIN_DICER/)) {
-            imagePumpkin.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.size, global.hoeutils.imageData.size);
+            imagePumpkin.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.scale);
         } else if (heldItem.getString('id').match(/MELON_DICER/)) {
-            imageMelon.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.size, global.hoeutils.imageData.size);
+            imageMelon.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.scale);
         } else if (heldItem.getString('id').match(/COCO_CHOPPER/)) {
-            imageCocoa.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.size, global.hoeutils.imageData.size);
+            imageCocoa.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.scale);
         } else if (heldItem.getString('id').match(/CACTUS_KNIFE/)) {
-            imageCactus.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.size, global.hoeutils.imageData.size);
+            imageCactus.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.scale);
         } else if (heldItem.getString('id').match(/FUNGI_CUTTER/)) {
-            imageMushroom.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.size, global.hoeutils.imageData.size);
+            imageMushroom.draw(global.hoeutils.data.hud.x - global.hoeutils.imageData.size, global.hoeutils.data.hud.y + global.hoeutils.imageData.yOffset, global.hoeutils.imageData.scale);
         }
     } catch (error) {
         updateImageData();
@@ -81,6 +59,6 @@ export function timerImage() {
     if (!global.hoeutils.userSettings.isEventTimerEnabled) return;
     if (!Scoreboard.getTitle().replace(/§./g, '').equals('SKYBLOCK') && !global.hoeutils.userSettings.isEventTimerEnabledEverywhere) return;
     if (global.hoeutils.userSettings.isEventImageEnabled) {
-        imageEvent.draw(global.hoeutils.data.timer.x - global.hoeutils.eventImageData.size - 4, global.hoeutils.data.timer.y + global.hoeutils.eventImageData.yOffset, global.hoeutils.eventImageData.size, global.hoeutils.eventImageData.size)
+        imageEvent.draw(global.hoeutils.data.timer.x - global.hoeutils.eventImageData.size - 4, global.hoeutils.data.timer.y + global.hoeutils.eventImageData.yOffset, global.hoeutils.eventImageData.scale)
     }
 }
